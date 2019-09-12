7fc05effbe67e14d46520fbcb682a11c30237077
Silas Zehnder <szehnder@umich.edu>
2019-09-12 10:17:23 -0600

    added opcode info

--- mos6502.cpp
+++ mos6502.cpp
@@ -9,171 +9,227 @@ mos6502::mos6502() {
 void mos6502::op_noop() {
     printf("FUTURE EXPANSION");
 }
+
 void mos6502::op_adc() {
 
 }
+
 void mos6502::op_and() {
 
 }
+
 void mos6502::op_asl() {
 
 }
+
 void mos6502::op_bcc() {
 
 }
+
 void mos6502::op_bcs() {
 
 }
+
 void mos6502::op_beq() {
 
 }
+
 void mos6502::op_bit() {
 
 }
+
 void mos6502::op_bmi() {
 
 }
+
 void mos6502::op_bne() {
 
 }
+
 void mos6502::op_bpl() {
 
 }
+
 void mos6502::op_brk() {
 
 }
+
 void mos6502::op_bvc() {
 
 }
+
 void mos6502::op_bvs() {
 
 }
+
 void mos6502::op_clc() {
 
 }
+
 void mos6502::op_cld() {
 
 }
+
 void mos6502::op_cli() {
 
 }
+
 void mos6502::op_clv() {
 
 }
+
 void mos6502::op_cmp() {
 
 }
+
 void mos6502::op_cpx() {
 
 }
+
 void mos6502::op_cpy() {
 
 }
+
 void mos6502::op_dec() {
 
 }
+
 void mos6502::op_dex() {
 
 }
+
 void mos6502::op_dey() {
 
 }
+
 void mos6502::op_eor() {
 
 }
+
 void mos6502::op_inc() {
 
 }
+
 void mos6502::op_inx() {
 
 }
+
 void mos6502::op_iny() {
 
 }
+
 void mos6502::op_jmp() {
 
 }
+
 void mos6502::op_jsr() {
 
 }
+
 void mos6502::op_lda() {
 
 }
+
 void mos6502::op_ldx() {
 
 }
+
 void mos6502::op_ldy() {
 
 }
+
 void mos6502::op_lsr() {
 
 }
+
 void mos6502::op_nop() {
 
 }
+
 void mos6502::op_ora() {
 
 }
+
 void mos6502::op_pha() {
 
 }
+
 void mos6502::op_php() {
 
 }
+
 void mos6502::op_pla() {
 
 }
+
 void mos6502::op_plp() {
 
 }
+
 void mos6502::op_rol() {
 
 }
+
 void mos6502::op_ror() {
 
 }
+
 void mos6502::op_rti() {
 
 }
+
 void mos6502::op_rts() {
 
 }
+
 void mos6502::op_sbc() {
 
 }
+
 void mos6502::op_sec() {
 
 }
+
 void mos6502::op_sed() {
 
 }
+
 void mos6502::op_sei() {
 
 }
+
 void mos6502::op_sta() {
 
 }
+
 void mos6502::op_stx() {
 
 }
+
 void mos6502::op_sty() {
 
 }
+
 void mos6502::op_tax() {
 
 }
+
 void mos6502::op_tay() {
 
 }
+
 void mos6502::op_tya() {
 
 }
+
 void mos6502::op_tsx() {
 
 }
+
 void mos6502::op_txa() {
 
 }
+
 void mos6502::op_txs() {
 
 }
--- mos6502.h
+++ mos6502.h
@@ -130,256 +130,256 @@ private:
 
     void (mos6502::*ops[256])() {
         &mos6502::op_brk,
-        &mos6502::op_ora,
+        &mos6502::op_ora,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_ora,
-        &mos6502::op_asl,
+        &mos6502::op_ora,           // Zero Page
+        &mos6502::op_asl,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_php,
-        &mos6502::op_ora,
-        &mos6502::op_asl,
+        &mos6502::op_ora,           // Immediate
+        &mos6502::op_asl,           // Accumulator
         &mos6502::op_noop,
-        &mos6502::op_bpl,
-        &mos6502::op_ora,
+        &mos6502::op_bpl,           //
+        &mos6502::op_ora,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_ora,
-        &mos6502::op_asl,
+        &mos6502::op_ora,           // Zero Page, X
+        &mos6502::op_asl,           // Zero Page, X
         &mos6502::op_noop,
         &mos6502::op_clc,
-        &mos6502::op_ora,
+        &mos6502::op_ora,           // Absolute, Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_ora,
-        &mos6502::op_asl,
+        &mos6502::op_ora,           // Absolute, X
+        &mos6502::op_asl,           // Absolute, X
         &mos6502::op_noop,
         &mos6502::op_jsr,
-        &mos6502::op_and,
+        &mos6502::op_and,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_bit,
-        &mos6502::op_and,
-        &mos6502::op_rol,
+        &mos6502::op_bit,           // Zero Page
+        &mos6502::op_and,           // Zero Page
+        &mos6502::op_rol,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_plp,
-        &mos6502::op_and,
-        &mos6502::op_rol,
+        &mos6502::op_and,           // Immediate
+        &mos6502::op_rol,           // Accumulator
         &mos6502::op_noop,
-        &mos6502::op_bit,
-        &mos6502::op_and,
-        &mos6502::op_rol,
+        &mos6502::op_bit,           // Absolute
+        &mos6502::op_and,           // Absolute
+        &mos6502::op_rol,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_bmi,
-        &mos6502::op_and,
+        &mos6502::op_and,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_and,
-        &mos6502::op_rol,
+        &mos6502::op_and,           // Zero Page, X
+        &mos6502::op_rol,           // Zero Page, X
         &mos6502::op_noop,
         &mos6502::op_sec,
-        &mos6502::op_and,
+        &mos6502::op_and,           // Absolute, Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_and,
-        &mos6502::op_rol,
+        &mos6502::op_and,           // Absolute, X
+        &mos6502::op_rol,           // Absolute, X
         &mos6502::op_noop,
         &mos6502::op_rti,
-        &mos6502::op_eor,
+        &mos6502::op_eor,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_eor,
-        &mos6502::op_lsr,
+        &mos6502::op_eor,           // Zero Page
+        &mos6502::op_lsr,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_pha,
-        &mos6502::op_eor,
-        &mos6502::op_lsr,
+        &mos6502::op_eor,           // Immediate
+        &mos6502::op_lsr,           // Accumulator
         &mos6502::op_noop,
-        &mos6502::op_jmp,
-        &mos6502::op_eor,
-        &mos6502::op_lsr,
+        &mos6502::op_jmp,           // Absolute
+        &mos6502::op_eor,           // Absolute
+        &mos6502::op_lsr,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_bvc,
-        &mos6502::op_eor,
+        &mos6502::op_eor,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_eor,
-        &mos6502::op_lsr,
+        &mos6502::op_eor,           // Zero Page, X
+        &mos6502::op_lsr,           // Zero Page, X
         &mos6502::op_noop,
         &mos6502::op_cli,
-        &mos6502::op_eor,
+        &mos6502::op_eor,           // Absolute, Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_eor,
-        &mos6502::op_lsr,
+        &mos6502::op_eor,           // Absolute, X
+        &mos6502::op_lsr,           // Absolute, X
         &mos6502::op_noop,
         &mos6502::op_rts,
-        &mos6502::op_adc,
+        &mos6502::op_adc,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_adc,
-        &mos6502::op_ror,
+        &mos6502::op_adc,           // Zero Page
+        &mos6502::op_ror,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_pla,
-        &mos6502::op_adc,
-        &mos6502::op_ror,
+        &mos6502::op_adc,           // Immediate
+        &mos6502::op_ror,           // Accumulator
         &mos6502::op_noop,
-        &mos6502::op_jmp,
-        &mos6502::op_adc,
-        &mos6502::op_ror,
+        &mos6502::op_jmp,           // Indirect
+        &mos6502::op_adc,           // Absolute
+        &mos6502::op_ror,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_bvs,
-        &mos6502::op_adc,
+        &mos6502::op_adc,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_adc,
-        &mos6502::op_ror,
+        &mos6502::op_adc,           // Zero Page, X
+        &mos6502::op_ror,           // Zero Page, X
         &mos6502::op_noop,
         &mos6502::op_sei,
-        &mos6502::op_adc,
+        &mos6502::op_adc,           // Absolute, Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_adc,
-        &mos6502::op_ror,
+        &mos6502::op_adc,           // Absolute, X
+        &mos6502::op_ror,           // Absolute, X
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_sta,
+        &mos6502::op_sta,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_sty,
-        &mos6502::op_sta,
-        &mos6502::op_stx,
+        &mos6502::op_sty,           // Zero Page
+        &mos6502::op_sta,           // Zero Page
+        &mos6502::op_stx,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_dey,
         &mos6502::op_noop,
         &mos6502::op_txa,
         &mos6502::op_noop,
-        &mos6502::op_sty,
-        &mos6502::op_sta,
-        &mos6502::op_stx,
+        &mos6502::op_sty,           // Absolute
+        &mos6502::op_sta,           // Absolute
+        &mos6502::op_stx,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_bcc,
-        &mos6502::op_sta,
+        &mos6502::op_sta,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_sty,
-        &mos6502::op_sta,
-        &mos6502::op_stx,
+        &mos6502::op_sty,           // Zero Page, X
+        &mos6502::op_sta,           // Zero Page, X
+        &mos6502::op_stx,           // Zero Page, Y
         &mos6502::op_noop,
         &mos6502::op_tya,
-        &mos6502::op_sta,
+        &mos6502::op_sta,           // Absolute, Y
         &mos6502::op_txs,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_sta,
+        &mos6502::op_sta,           // Absolute, X
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_ldy,
-        &mos6502::op_lda,
-        &mos6502::op_ldx,
+        &mos6502::op_ldy,           // Immediate
+        &mos6502::op_lda,           // (Indirect, X)
+        &mos6502::op_ldx,           // Immediate
         &mos6502::op_noop,
-        &mos6502::op_ldy,
-        &mos6502::op_lda,
-        &mos6502::op_ldx,
+        &mos6502::op_ldy,           // Zero Page
+        &mos6502::op_lda,           // Zero Page
+        &mos6502::op_ldx,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_tay,
-        &mos6502::op_lda,
+        &mos6502::op_lda,           // Immediate
         &mos6502::op_tax,
         &mos6502::op_noop,
-        &mos6502::op_ldy,
-        &mos6502::op_lda,
-        &mos6502::op_ldx,
+        &mos6502::op_ldy,           // Absolute
+        &mos6502::op_lda,           // Absolute
+        &mos6502::op_ldx,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_bcs,
-        &mos6502::op_lda,
+        &mos6502::op_lda,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_ldy,
-        &mos6502::op_lda,
-        &mos6502::op_ldx,
+        &mos6502::op_ldy,           // Zero Page, X
+        &mos6502::op_lda,           // Zero Page, X
+        &mos6502::op_ldx,           // Zero Page, Y
         &mos6502::op_noop,
         &mos6502::op_clv,
-        &mos6502::op_lda,
+        &mos6502::op_lda,           // Absolute, Y
         &mos6502::op_tsx,
         &mos6502::op_noop,
-        &mos6502::op_ldy,
-        &mos6502::op_lda,
-        &mos6502::op_ldx,
+        &mos6502::op_ldy,           // Absolute, X
+        &mos6502::op_lda,           // Absolute, X
+        &mos6502::op_ldx,           // Absolute, Y
         &mos6502::op_noop,
-        &mos6502::op_cpy,
-        &mos6502::op_cmp,
+        &mos6502::op_cpy,           // Immediate
+        &mos6502::op_cmp,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_cpy,
-        &mos6502::op_cmp,
-        &mos6502::op_dec,
+        &mos6502::op_cpy,           // Zero Page
+        &mos6502::op_cmp,           // Zero Page
+        &mos6502::op_dec,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_iny,
-        &mos6502::op_cmp,
+        &mos6502::op_cmp,           // Immediate
         &mos6502::op_dex,
         &mos6502::op_noop,
-        &mos6502::op_cpy,
-        &mos6502::op_cmp,
-        &mos6502::op_dec,
+        &mos6502::op_cpy,           // Absolute
+        &mos6502::op_cmp,           // Absolute
+        &mos6502::op_dec,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_bne,
-        &mos6502::op_cmp,
+        &mos6502::op_cmp,           // (Indrect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_cmp,
-        &mos6502::op_dec,
+        &mos6502::op_cmp,           // Zero Page, X
+        &mos6502::op_dec,           // Zero Page, X
         &mos6502::op_noop,
         &mos6502::op_cld,
-        &mos6502::op_cmp,
+        &mos6502::op_cmp,           // Absolute, Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_cmp,
-        &mos6502::op_dec,
+        &mos6502::op_cmp,           // Absolute, X
+        &mos6502::op_dec,           // Absolute, X
         &mos6502::op_noop,
-        &mos6502::op_cpx,
-        &mos6502::op_sbc,
+        &mos6502::op_cpx,           // Immediate
+        &mos6502::op_sbc,           // (Indirect, X)
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_cpx,
-        &mos6502::op_sbc,
-        &mos6502::op_inc,
+        &mos6502::op_cpx,           // Zero Page
+        &mos6502::op_sbc,           // Zero Page
+        &mos6502::op_inc,           // Zero Page
         &mos6502::op_noop,
         &mos6502::op_inx,
-        &mos6502::op_sbc,
+        &mos6502::op_sbc,           // Immediate
         &mos6502::op_nop,
         &mos6502::op_noop,
-        &mos6502::op_cpx,
-        &mos6502::op_sbc,
-        &mos6502::op_inc,
+        &mos6502::op_cpx,           // Absolute
+        &mos6502::op_sbc,           // Absolute
+        &mos6502::op_inc,           // Absolute
         &mos6502::op_noop,
         &mos6502::op_beq,
-        &mos6502::op_sbc,
+        &mos6502::op_sbc,           // (Indirect), Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_sbc,
-        &mos6502::op_inc,
+        &mos6502::op_sbc,           // Zero Page, X
+        &mos6502::op_inc,           // Zero Page, X
         &mos6502::op_noop,
         &mos6502::op_sed,
-        &mos6502::op_sbc,
+        &mos6502::op_sbc,           // Absolute, Y
         &mos6502::op_noop,
         &mos6502::op_noop,
         &mos6502::op_noop,
-        &mos6502::op_sbc,
-        &mos6502::op_inc,
+        &mos6502::op_sbc,           // Absolute, X
+        &mos6502::op_inc,           // Absolute, X
         &mos6502::op_noop
     };
 };
